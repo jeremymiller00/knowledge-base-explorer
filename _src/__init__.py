9c39ebb77cf41b647b9b4a2f3bee1b2ede3f009b
Jeremy Miller <jeremymiller00@gmail.com>
2024-12-09 17:39:02 -0800

    update layout

--- _src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-https://stackoverflow.com/questions/73381902/running-unittest-with-modules-that-must-import-other-modules
-"""
-
-import sys
-
-import project.utils as utils
-sys.modules['utils'] = utils
